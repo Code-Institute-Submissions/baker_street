6be28b8040fac479cc580f035d7f2672cdaf5cb5
Nick <nickbaker11@gmail.com>
2020-10-26 11:43:48 +0000

    our_rooms, contact + leaderboards html with views and urls added

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('rooms', views.our_rooms, name='our_rooms'),
+    path('contact', views.contact, name='contact'),
+    path('leaderboards', views.leaderboards, name='leaderboards'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'home/index.html')
+
+
+def our_rooms(request):
+    return render(request, 'home/rooms.html')
+
+
+def contact(request):
+    return render(request, 'home/contact.html')
+
+
+def leaderboards(request):
+    return render(request, 'home/leaderboards.html')
