e526f2aafff73c33f84433948969f29f666d71fd
Nick <nickbaker11@gmail.com>
2020-10-28 15:36:15 +0000

    Created bookings app with view and urls. Created Rooms model for bookings

---
+++ bookings/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Rooms(models.Model):
+    room_title = models.CharField(max_length=254, null=False, blank=False)
+    price = models.DecimalField(max_digits=5, decimal_places=2, null=False, blank=False)  # noqa:E501
+    room_description = models.CharField(max_length=254, null=False, blank=False)  # noqa:E501
+    room_image = models.ImageField(null=False, blank=False)
+
+    def __str__(self):
+        return super().__str__()
