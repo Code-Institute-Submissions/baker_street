ba193b0172218b36a0b0c3f954eca59da61c6727
Nick <nickbaker11@gmail.com>
2020-11-09 14:59:14 +0000

    Added bookings template for each room

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,7 +1,9 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('bookings', views.bookings, name='bookings'),
+    path('bookings_watson', views.bookings_watson, name='bookings_watson'),
+    path('bookings_pink', views.bookings_pink, name='bookings_pink'),
+    path('bookings_moriarty', views.bookings_moriarty, name='bookings_moriarty'),  # noqa: E501
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -1,5 +1,46 @@
 from django.shortcuts import render
+from .models import Rooms
 
 
 def bookings(request):
-    return render(request, 'bookings/bookings.html')
+
+    rooms = Rooms.objects.all()
+    print(rooms)
+    context = {
+        'rooms': rooms,
+    }
+
+    return render(request, 'bookings/bookings.html', context)
+
+
+def bookings_watson(request):
+
+    watson = Rooms.objects.filter(pk=1)
+    print(watson)
+    context = {
+        'watson': watson,
+    }
+
+    return render(request, 'bookings/bookings_watson.html', context)
+
+
+def bookings_pink(request):
+
+    pink = Rooms.objects.filter(pk=2)
+
+    context = {
+        'pink': pink,
+    }
+
+    return render(request, 'bookings/bookings_pink.html', context)
+
+
+def bookings_moriarty(request):
+
+    moriarty = Rooms.objects.filter(pk=3)
+
+    context = {
+        'moriarty': moriarty,
+    }
+
+    return render(request, 'bookings/bookings_moriarty.html', context)
