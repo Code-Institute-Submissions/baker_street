64430faf9affc5a9200399131466337a01fc60af
Nick <nickbaker11@gmail.com>
2020-12-05 13:55:33 +0000

    Delete bag items function working, but url needs editing from bag/add to bag

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from django.contrib import messages
+from bookings.models import Rooms, Room_Booking
 
 
 def bag(request):
@@ -24,9 +26,41 @@ def add_to_bag(request, item_id):
 
 
 def delete_from_bag(request, item_id):
-    if 'item_id' in request.POST:
+    print("------------------------ request.POST === ", request.POST)
+    print("------------------------ item_id === ", item_id)
+    print("------------------------ request === ", request)
+    try:
+        room = get_object_or_404(Rooms, pk=item_id)
         bag = request.session.get('bag', {})
         bag.pop(item_id)
+        messages.success(request, (f'Removed {room.room_title} from your bag'))
         request.session['bag'] = bag
-        return render(request, 'bag/bag.html')
-        print(delete_from_bag)
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
+
+
+# def adjust_bag(request, item_id):
+#     num_of_players = int(request.POST.get('num_of_players'))
+#     date = (request.POST.get('date'))
+#     time = (request.POST.get('time'))
+#     if 'product_size' in request.POST:
+#         size = request.POST['product_size']
+#     bag = request.session.get('bag', {})
+
+#     if size:
+#         if quantity > 0:
+#             bag[item_id]['items_by_size'][size] = quantity
+#         else:
+#             del bag[item_id]['items_by_size'][size]
+#             if not bag[item_id]['items_by_size']:
+#                 bag.pop(item_id)
+#     else:
+#         if quantity > 0:
+#             bag[item_id] = quantity
+#         else:
+#             bag.pop(item_id)
+
+#     request.session['bag'] = bag
+#     return redirect(reverse('bag'))
