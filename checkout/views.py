10daf08b19115774000d994e283e2f96a91e4024
Nick <nickbaker11@gmail.com>
2020-12-14 22:09:49 +0000

    Card details box added to checkout.html. Styling needed

--- checkout/views.py
+++ checkout/views.py
@@ -6,6 +6,8 @@ def checkout(request):
 
     orderinfo = PersonalInfoOrder
     context = {
-        'orderinfo': orderinfo
+        'orderinfo': orderinfo,
+        'stripe_public_key': 'pk_test_51HyFP7JEsTpKioAIGAWyLYhF6xW4qYFPKVLiDAZewMG9Ilwt885rOkMwPKqRztFqD9fXj25uXG3nwRGn1dDkiD3O00w5kF0dK0',  # noqa: E501
+        'client_secret': 'test client secret'
     }
     return render(request, 'checkout/checkout.html', context)
