d6495e7b71547892c161fdeab5194d2815fc769d
Nick <nickbaker11@gmail.com>
2021-01-06 17:11:11 +0000

    Calendar altered to prevent previous dates being selected

--- bookings/forms.py
+++ bookings/forms.py
@@ -1,12 +1,20 @@
 from django import forms
 from .models import Room_Booking
+from datetime import datetime
 
 
 class ViewCalendar(forms.DateInput):
     input_type = 'date'
 
 
+today = datetime.today().strftime('%Y-%m-%d')
+
+
 class Booking(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(Booking, self).__init__(*args, **kwargs)
+        self.fields['date'].widget.attrs['min'] = today
+
     class Meta:
         model = Room_Booking
         fields = ['num_of_players', 'date', 'time']
